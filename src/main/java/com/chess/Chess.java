3400f246b8525bfbb5f7e2a20c1161d40fdb2375
Chester Ken Gallego <chesterkengallego@gmail.com>
2022-11-22 16:09:54 +0800

    Add com.chess package

---
+++ src/main/java/com/chess/Chess.java
@@ -0,0 +1,15 @@
+/**
+ * A class representing a chess game.
+ *
+ * @author Kurt Matthew Amodia
+ * @author Chester Ken Gallego
+ * @author Ervene Jhay Gambito
+ */
+
+package com.chess;
+
+public class Chess {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
