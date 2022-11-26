bb6de6b244c44bb552d7470c6fc60b775fe9400a
Chester Ken Gallego <chesterkengallego@gmail.com>
2022-11-26 10:27:40 +0800

    Update Players.java

--- src/main/java/com/chess/players/Players.java
+++ src/main/java/com/chess/players/Players.java
@@ -10,4 +10,29 @@
 package com.chess.players;
 
 public class Players {
+    // Set the first player to white
+    private static String currentPlayer = "White";
+
+    // Get the current player
+    public static String getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    // Get the next player
+    public static String getNextPlayer() {
+        if (currentPlayer.equals("White")) {
+            return "Black";
+        } else {
+            return "White";
+        }
+    }
+
+    // Switch the current player to the next player
+    public static void switchCurrentPlayer() {
+        if (currentPlayer.equals("White")) {
+            currentPlayer = "Black";
+        } else {
+            currentPlayer = "White";
+        }
+    }
 }
