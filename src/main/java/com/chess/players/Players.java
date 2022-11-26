68f902ed9d89c1e65a9d8692cfcf4937b2f7ca98
Chester Ken Gallego <chesterkengallego@gmail.com>
2022-11-26 10:27:24 +0800

    Add com.chess.players package

---
+++ src/main/java/com/chess/players/Players.java
@@ -0,0 +1,13 @@
+/**
+ * Players.java
+ * A class that represents the players in a chess game.
+ *
+ * @author Kurt Matthew Amodia
+ * @author Chester Ken Gallego
+ * @author Ervene Jhay Gambito
+ */
+
+package com.chess.players;
+
+public class Players {
+}
