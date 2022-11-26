5c2967d3b76e50b4d43605f75dba4e696e107094
Chester Ken Gallego <chesterkengallego@gmail.com>
2022-11-26 10:31:04 +0800

    Update ChessPrinter.java

--- src/main/java/com/chess/util/ChessPrinter.java
+++ src/main/java/com/chess/util/ChessPrinter.java
@@ -10,4 +10,25 @@
 package com.chess.util;
 
 public class ChessPrinter {
+    // Initialize the ANSI escape codes
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+
+    // Clear the terminal screen
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    // Print the welcome message
+    public static void printWelcomeMessage() {
+        System.out.println("Welcome to Chess!");
+    }
+
+    // Print the current player
+    public static void printCurrentPlayer(String currentPlayer) {
+        System.out.println("Current player: " + currentPlayer);
+    }
 }
