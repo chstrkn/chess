35ba6efa52c862fec9940454cf28b291083aeaf8
Chester Ken Gallego <chesterkengallego@gmail.com>
2022-11-26 09:59:54 +0800

    Add com.chess.chessboard package

---
+++ src/main/java/com/chess/chessboard/Chessboard.java
@@ -0,0 +1,13 @@
+/**
+ * Chessboard.java
+ * A class that represents a chessboard in a chess game.
+ *
+ * @author Kurt Matthew Amodia
+ * @author Chester Ken Gallego
+ * @author Ervene Jhay Gambito
+ */
+
+package com.chess.chessboard;
+
+public class Chessboard {
+}
