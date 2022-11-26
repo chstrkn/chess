b7af0582010a41c0a6161f6308af744a7a9fc175
Chester Ken Gallego <chesterkengallego@gmail.com>
2022-11-26 10:24:36 +0800

    Add com.chess.util package

---
+++ src/main/java/com/chess/util/ChessPrinter.java
@@ -0,0 +1,13 @@
+/**
+ * ChessPrinter.java
+ * A class that contains the methods used to print the chess game to the terminal.
+ *
+ * @author Kurt Matthew Amodia
+ * @author Chester Ken Gallego
+ * @author Ervene Jhay Gambito
+ */
+
+package com.chess.util;
+
+public class ChessPrinter {
+}
---
+++ src/main/java/com/chess/util/UserInputHandler.java
@@ -0,0 +1,13 @@
+/**
+ * UserInputHandler.java
+ * A class that contains the methods used to handle user input.
+ *
+ * @author Kurt Matthew Amodia
+ * @author Chester Ken Gallego
+ * @author Ervene Jhay Gambito
+ */
+
+package com.chess.util;
+
+public class UserInputHandler {
+}
